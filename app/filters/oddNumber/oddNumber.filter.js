aa88b6431aaf35e35d4f5f9aed9c1fb2152a134b
likvi85 <dshykaljuk@gmail.com>
2016-04-28 20:55:34 +0200

    fix filter tests

---
+++ app/filters/oddNumber/oddNumber.filter.js
@@ -0,0 +1,15 @@
+(function(angular) {
+  'use strict';
+
+  angular
+  .module('angularApp')
+  .filter('oddNumber', oddNumberFilter);
+
+  function oddNumberFilter() {
+    return function(items) {
+      return items.filter(function(item) {
+        return item % 2 != 0;
+      });
+    };
+  }
+})(window.angular);
---
+++ app/filters/oddNumber/oddNumber.filter.spec.js
@@ -0,0 +1,22 @@
+describe('oddNumber filter', function () {
+  'use strict';
+
+  var oddNumberFilter;
+
+  beforeEach(module('angularApp'));
+
+  beforeEach(inject(function($filter) {
+    oddNumberFilter = $filter('oddNumber');
+
+  }));
+
+  describe('When call filter on array of digits', function () {
+    var oddNumbers;
+    beforeEach(function () {
+      oddNumbers = oddNumberFilter([1,2,3,4,5])
+    });
+    it('Should return array with only odd number', function () {
+      expect(oddNumbers.length).toBe(3);
+    });
+  });
+});
